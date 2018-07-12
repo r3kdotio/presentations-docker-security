6ab2e352e6ee45c4a1b79735ece7ed0863c92856
Richard Kolb <richardk@hiliosits.com>
2018-07-12 16:20:52 +0200

    extra log in c file. Move bit-coin-mine

--- use-all-memory.c
+++ use-all-memory.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 int main() {
-  printf("BEWARE : about to consume all memory. Without limits this will crash your system!!");
+  printf("BEWARE : about to consume all memory");
+  int count = 0;
   while(1==1){
+    printf("Consume ram "+ count ++);
     malloc( 1024*1024*1024*1024 );
   }
 }
-
